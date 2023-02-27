017d2cc8c1f1419f90546e90ed22aa95ab561fed
DucVuMinh <depzai28030712@gmail.com>
2023-02-27 10:21:48 +0700

    Updated

--- st_aes.py
+++ st_aes.py
@@ -1,24 +1,24 @@
 import streamlit as st
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
-
+import io
 
 st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 
 
-col1, col2 = st.columns(2, gap = 'large')
+col1, col2 = st.columns(2, gap='large')
 
 #  ENCODE
 with col1:
-    
+
     st.header('Online Encryption')
     st.subheader('AES mode: ECB')
-    password =  st.text_input(label = "Enter password", key = 1, type = 'password')
-    
+    password = st.text_input(label="Enter password", key=1, type='password')
+
     #  bien password thanh 256 bits
     hash_obj = SHA256.new(password.encode('utf-8'))
     hkey = hash_obj.digest()
-    
+
     # ham encrypt
     def encrypt(info):
         message = info
@@ -29,7 +29,7 @@ with col1:
         result = cipher.encrypt(padding(message).encode('utf-8'))
         return result
 
-    msg = st.text_input('Enter text to be encrypted', key =2)
+    msg = st.text_input('Enter text to be encrypted', key=2)
     if st.button("Encrypt"):
 
         cipher_text = encrypt(msg)
@@ -38,17 +38,19 @@ with col1:
         pw = f'- Password is:\n{password}\n\n- The 256-bits form is:\n{hkey}'
 
         contents = f'{hex_version}\n\n{bform}\n\n{pw}'
+
+        # Save the encrypted text as bytes when the download button is clicked
+        bytes_io = io.BytesIO(contents.encode())
         st.write("Text has been encrypted, click download button to save")
-        tai = st.download_button(label = 'Download to file', data= contents, file_name= 'encrypted_text.txt')
-        
-    
+        tai = st.download_button(label='Download to file', data=bytes_io.getvalue(), file_name='encrypted_text.txt')
+
+
 #  DECODE
 with col2:
-    
+
     st.header('Online Decryption')
     st.subheader('AES mode: ECB')
-    
-        
+
     def decrypt(info):
         message = info
         pad = "{"
@@ -57,31 +59,25 @@ with col2:
         pad_index = plaintext.find(pad)
         result = plaintext[:pad_index]
         return result
-    
+
     try:
-        password = st.text_input(label = 'Enter password', key = 3, type = 'password')
+        password = st.text_input(label='Enter password', key=3, type='password')
         hash_obj = SHA256.new(password.encode('utf-8'))
         hkey = hash_obj.digest()
+
         # Upload file
         file = st.file_uploader("Upload file")
         if file is not None:
-        # Read lines from file
+            # Read lines from file
             lines = file.readlines()
-            
-            #password = lines[7].decode().strip('\n')
-            #hash_obj = SHA256.new(password.encode('utf-8'))
-           # hkey = hash_obj.digest()
-            
+
             encrypted_text = lines[1].decode().strip('\n')
             text_bytes = bytes.fromhex(encrypted_text)
             pt = decrypt(text_bytes)
             st.text_area('The plain text', pt)
-
-        
 
     except UnicodeDecodeError as e1:
         st.error('There was an error because the password or the input has been changed')
 
     except ValueError as e2:
         st.error('There was an error because the password or the input has been changed')
-    
