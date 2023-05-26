17d1464b1b78348f53fb646af61daabdfb8c35fa
DucVuMinh <depzai28030712@gmail.com>
2023-05-27 06:39:45 +0700

    27/5 updated

--- st_aes.py
+++ st_aes.py
@@ -1,7 +1,10 @@
 import streamlit as st
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
 from PIL import Image
+import time
 
 # Set page configuration
 
@@ -21,29 +24,26 @@ textColor="#252525"
 def encrypt_text(password, plaintext):
     hash_obj = SHA256.new(password.encode('utf-8'))
     hkey = hash_obj.digest()
-    message = plaintext
-    block_size = 16
-    pad = "{"
-    padding = lambda s: s + (block_size - len(message) % block_size) * pad
-    cipher = AES.new(hkey, AES.MODE_ECB)
-    result = cipher.encrypt(padding(message).encode('utf-8'))
-    return result
+    iv = get_random_bytes(16)
+    cipher = AES.new(hkey, AES.MODE_CBC, iv)
+    ciphertext = cipher.encrypt(pad(plaintext.encode('utf-8'), 16))
+    return iv + ciphertext
 
 # Define function to decrypt text
-def decrypt_text(password, ciphertext):
+def decrypt_text(password, ciphertext, iv):
     hash_obj = SHA256.new(password.encode('utf-8'))
     hkey = hash_obj.digest()
-    message = ciphertext
-    pad = "{"
-    decipher = AES.new(hkey, AES.MODE_ECB)
-    plaintext = decipher.decrypt(message).decode('utf-8')
-    pad_index = plaintext.find(pad)
-    result = plaintext[:pad_index]
-    return result
+    ciphertext = text_bytes
+    cipher = AES.new(hkey, AES.MODE_CBC, iv)
+    plaintext = unpad(cipher.decrypt(ciphertext), 16).decode('utf-8')
+    return plaintext
+
 
 # Set up sidebar with page title and instructions
 st.sidebar.title("AES Encryption/Decryption")
 st.sidebar.write("This is a simple tool for encrypting and decrypting text using AES encryption. Enter a password and the text you want to encrypt, then click the 'Encrypt' button. To decrypt, enter the password and upload the encrypted text file.")
+st.sidebar.title("Cổng dịch vụ mã hóa, giải mã AES")
+st.sidebar.write("Đây là một công cụ đơn giản để mã hóa và giải mã văn bản bằng mã hóa AES. Nhập mật khẩu và văn bản bạn muốn mã hóa, sau đó nhấp vào nút 'Mã hóa'. Để giải mã, hãy nhập mật khẩu và tải lên tệp văn bản được mã hóa.")
 
 # Set up main page layout
 header = st.container()
@@ -54,7 +54,7 @@ decryption = st.container()
 with header:
     st.image(lock_image, width=100)
     st.title("AES Encryption/Decryption")
-    st.subheader("Mode using: ECB")
+    st.subheader("Mode using: CBC")
 
 # Set up encryption section with input fields and button
 with encryption:
@@ -63,14 +63,26 @@ with encryption:
     plaintext = st.text_area("Enter text to be encrypted")
     if password and plaintext:
         if st.button("Encrypt"):
-            ciphertext = encrypt_text(password, plaintext)
-            st.success("Text has been encrypted. Click 'Download' to save the encrypted text to a file.")
-            
-            hex_version = f'- The hex form of encoded text:\n{ciphertext.hex()}'
-            bform = f'- The bytes form of encoded text:\n{ciphertext}'
-            pw = f'- Password is:\n{password}'
-            contents = f'{hex_version}\n\n{bform}\n\n{pw}'
-            st.download_button(label="Download", data=contents, file_name="encrypted_text.txt")
+            try:
+                #start_time = time.perf_counter()
+                ciphertext = encrypt_text(password, plaintext)
+                print(ciphertext)
+            #     end_time = time.perf_counter()
+
+            # # Calculate the running time in milliseconds
+            #     running_time_ms = (end_time - start_time) *1000
+            #     print("Decrypt - Running time:", running_time_ms, "milliseconds")
+            #     print('....')
+                st.success("Text has been encrypted. Click 'Download' to save the encrypted text to a file.")
+                
+                hex_version = f'- The hex form of encoded text:\n{ciphertext[16:].hex()}'
+                iv = f'The iv is:\n{ciphertext[:16]}'
+                bform = f'- The bytes form of encoded text:\n{ciphertext[16:]}'
+                pw = f'- Password is:\n{password}'
+                contents = f'{hex_version}\n\n{iv}\n\n{bform}\n\n{pw}'
+                st.download_button(label="Download", data=contents, file_name="encrypted_text.txt")
+            except Exception as e1:
+                st.error('There were some errors during the encryption. Please check the information again')
     else:
         st.warning("Please enter a password and text to be encrypted.")
 
@@ -81,11 +93,19 @@ with decryption:
     file = st.file_uploader("Upload encrypted text file")
     if file is not None:
         try:
+            ####start_time = time.perf_counter()
             # Read lines from file
             lines = file.readlines()
-            encrypted_text = lines[1].decode().strip('\n')
+            encrypted_text = lines[1].decode().strip('\n')  #  decode de chuyen chuoi b'' sang string truoc khi .fromhex
+            iv = eval(lines[4])
             text_bytes = bytes.fromhex(encrypted_text)
-            plaintext = decrypt_text(password, text_bytes)
+            plaintext = decrypt_text(password, text_bytes, iv)
+            ####end_time = time.perf_counter()
+
+            # Calculate the running time in milliseconds
+           #### running_time_ms = (end_time - start_time) *1000
+            ####print("Decrypt - Running time:", running_time_ms, "milliseconds")
+            ####print('....')
             st.success("Text has been decrypted.")
             st.text_area("Decrypted text", value=plaintext)
         except Exception as e:
